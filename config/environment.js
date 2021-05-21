95f39b1ac78d06943bcafec5a46e9ce91e2272b4
Jeff Manke <jmanke@esri.com>
2021-05-21 09:27:06 -0700

    more ENV config

--- config/environment.js
+++ config/environment.js
@@ -44,7 +44,8 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
-    // here you can enable a production-specific feature
+    ENV.baseURL = '/meta-tag-test';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
