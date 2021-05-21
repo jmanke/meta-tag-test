3ce8a9a08b2ad8e133366631322c87a7201fa31c
Jeff Manke <jmanke@esri.com>
2021-05-21 10:40:37 -0700

    testing

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,3 +1,30 @@
 import Route from '@ember/routing/route';
+// import taginsert from 'taginsert';
 
-export default class HomeRoute extends Route {}
+export default class HomeRoute extends Route {
+  model() {
+    // setTimeout(() => {
+    // const item = {
+    //   title: 'Meta Tags Test App',
+    //   description: 'This is to test meta tags being added in dynamically',
+    //   url: 'http://localhost:4200',
+    //   thumbnailUrl:
+    //     'https://www.arcgis.com/sharing/rest/content/items/4d63f7de557c4a69bab44c376c0d3565/info/thumbnail/thumbnail1564420707532.jpeg',
+    // };
+    // taginsert({
+    //   item,
+    //   mapping: {
+    //     w3c: {
+    //       image_src: 'thumbnailUrl',
+    //     },
+    //     twitter: {
+    //       image: 'thumbnailUrl',
+    //     },
+    //     og: {
+    //       image: 'thumbnailUrl',
+    //     },
+    //   },
+    // });
+    // }, 0);
+  }
+}
